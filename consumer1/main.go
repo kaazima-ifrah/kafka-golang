2f429473136f7a9e86cec18eba06542329bb314c
Kaazima Ifrah <mirza.ifrah@swiggy.in>
2022-08-08 12:57:58 +0530

    Add consumers

---
+++ consumer1/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/segmentio/kafka-go"
+	"log"
+)
+
+func main() {
+
+	topic := "example-topic-123"
+
+	r := kafka.NewReader(kafka.ReaderConfig{
+		Brokers: []string{"localhost:9092"},
+		GroupID: "consumer-group-1",
+		Topic:   topic,
+	})
+
+	for {
+		m, err := r.ReadMessage(context.Background())
+		if err != nil {
+			break
+		}
+		fmt.Printf("Message at offset %d: %s = %s\n", m.Offset, string(m.Key), string(m.Value))
+	}
+
+	if err := r.Close(); err != nil {
+		log.Fatal("Failed to close reader:", err)
+	}
+}
