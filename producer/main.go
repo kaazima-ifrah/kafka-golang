0ec76e154b8e75eef5bac1564b714a68e7de8570
Kaazima Ifrah <mirza.ifrah@swiggy.in>
2022-08-08 12:14:31 +0530

    Add producer

---
+++ producer/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/segmentio/kafka-go"
+	"log"
+)
+
+func main() {
+
+	w := &kafka.Writer{
+		Addr:                   kafka.TCP("localhost:9092"),
+		Topic:                  "example-123",
+		RequiredAcks:           kafka.RequireAll,
+		AllowAutoTopicCreation: false,
+		Async:                  true,
+		Completion: func(messages []kafka.Message, err error) {
+
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+
+			for _, val := range messages {
+				fmt.Printf("Message sent, offset %d, key %s, val %s \n", val.Offset, val.Key, val.Value)
+			}
+		},
+	}
+
+	err := w.WriteMessages(context.Background(),
+		kafka.Message{
+			Key:   []byte("message_name"),
+			Value: []byte("message1"),
+		},
+		kafka.Message{
+			Key:   []byte("message_name"),
+			Value: []byte("message2"),
+		},
+		kafka.Message{
+			Key:   []byte("message_name"),
+			Value: []byte("message3"),
+		},
+	)
+
+	if err != nil {
+		log.Fatal("Failed to write messages:", err)
+	}
+
+	if err := w.Close(); err != nil {
+		log.Fatal("Failed to close writer:", err)
+	}
+}
